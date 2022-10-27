a753a39266753d6b6539aadb63a76b148e653302
João Martins <jvmartinsdev@gmail.com>
2022-10-27 14:29:11 -0300

    :wrench: port with dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import { httpServer } from './http';
 import './webSocket.ts';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 httpServer.listen(process.env.PORT || 3000, () => console.log('Server is running on port 3000'));
