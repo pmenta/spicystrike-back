4fcca994a4b3827f93659e07a5c255ae158548b9
João Martins <jvmartinsdev@gmail.com>
2022-10-27 02:15:45 -0300

    :wrench: port to env

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import { httpServer } from './http';
 import './webSocket.ts';
 
-httpServer.listen(3000, () => console.log('Server is running on port 3000'));
+httpServer.listen(process.env.PORT || 3000, () => console.log('Server is running on port 3000'));
