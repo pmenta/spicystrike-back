aebd9cf315a8549355bb464fa9f0375adf7d4957
João Martins <jvmartinsdev@gmail.com>
2022-10-28 22:59:36 -0300

    :art: usign path mapping

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import { httpServer } from './http';
-import './webSocket.ts';
 import dotenv from 'dotenv';
+import { httpServer } from '@/http';
+import '@/webSocket';
 
 dotenv.config();
 
-httpServer.listen(process.env.PORT || 3000, () => console.log('Server is running on port 3000'));
+httpServer.listen(process.env.PORT || 3000, () => console.log(`Server is running on port ${process.env.PORT || 3000}`));
